182a33564c66cdb4bb0210afff939ac26cd343b8
stacy <sschauls2015@gmail.com>
2019-03-21 15:24:09 -0700

    moved stuff around. Going to start on exec and fork now for lab5

---
+++ FileSys/wget_samples.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# Variables
+PROTOCOL='https'
+EECS_SERVER='eecs.wsu.edu'
+CP_DIR='~cs460/samples/FILEsys'
+
+# -r recursive, no-parent guarantees we won't ascend to .., A accept list
+wget -r --no-parent -A '*' "$PROTOCOL://www.$EECS_SERVER/$CP_DIR/"
+cp "www.$EECS_SERVER/$CP_DIR/"* .
+rm -rf "www.$EECS_SERVER"
+rm -rf *.html*
