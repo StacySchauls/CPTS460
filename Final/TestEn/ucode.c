db423c295c3a4b64fab2ce8ac99e1dbfb8dd0ebc
stacy <sschauls2015@gmail.com>
2019-04-20 11:17:00 -0700

    changed ucode.c to include a strtok function

--- Final/TestEn/ucode.c
+++ Final/TestEn/ucode.c
@@ -575,35 +575,50 @@ int strcasecmp(char *s1, char *s2)
   return strcmp(t1, t2);
 }
 
-
-static char *olds;
-
-char * strtok (char *s, const char *delim)
+char * strtok(char * str, char *comp)
 {
-  char *token;
+	static int pos;
+	static char *s;	
+	int i =0, start = pos;
 
-  if (s == null)
-    s = olds;
-
-  /* Scan leading delimiters.  */
-  s += strspn (s, delim);
-  if (*s == '\0')
-    {
-      olds = s;
-      return null;
-    }
-
-  /* Find the end of the token.  */
-  token = s;
-  s = strpbrk (token, delim);
-  if (s == null)
-    /* This token finishes the string.  */
-    olds = __rawmemchr (token, '\0');
-  else
-    {
-      /* Terminate the token and make OLDS point past it.  */
-      *s = '\0';
-      olds = s + 1;
-    }
-  return token;
-}
+	// Copying the string for further calls of strtok
+	if(str!=NULL)
+		s = str;
+	
+	i = 0;
+	int j = 0;
+	//While not end of string
+	while(s[pos] != '\0')
+	{
+		j = 0;	
+		//Comparing of one of the delimiter matches the character in the string
+		while(comp[j] != '\0')
+		{		
+			//Pos point to the next location in the string that we have to read
+			if(s[pos] == comp[j])
+			{
+				//Replace the delimter by \0 to break the string
+				s[pos] = '\0';
+				pos = pos+1;				
+				//Checking for the case where there is no relevant string before the delimeter.
+				//start specifies the location from where we have to start reading the next character
+				if(s[start] != '\0')
+					return (&s[start]);
+				else
+				{
+					// Move to the next string after the delimiter
+					start = pos;
+					// Decrementing as it will be incremented at the end of the while loop
+					pos--;
+					break;
+				}
+			}
+			j++;
+		}
+		pos++;		
+	}//End of Outer while
+	s[pos] = '\0';
+	if(s[start] == '\0')
+		return NULL;
+	else
+		return &s[start];
